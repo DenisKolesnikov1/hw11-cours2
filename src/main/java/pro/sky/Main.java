1f4251436ffdea20eb20c072964b587373668a8e
Денис Колесников <73371887@mail.ru>
2024-06-16 12:41:11 +0300

    hw11

--- src/main/java/pro/sky/Main.java
+++ src/main/java/pro/sky/Main.java
@@ -1,7 +1,16 @@
 package pro.sky;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String lettersAndSpaceStr = "Petrov Ivan";
+        String emptyStr = " ";
+
+        System.out.println(StringUtils.capitalize(lettersAndSpaceStr));
+        System.out.println(StringUtils.deleteWhitespace(lettersAndSpaceStr));
+        System.out.println(StringUtils.substringAfterLast(lettersAndSpaceStr, " "));
+
     }
 }
